b37b3a61bf07617459b5762741410c3287f03713
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-05-27 22:13:44 +0700

    fix: update connection string to use SQL Server authentication

--- backend/MartialArtsClubManagement/MartialArtsClubManagement.API/Models/Entities/QuanLyCLBVoThuatContext.cs
+++ backend/MartialArtsClubManagement/MartialArtsClubManagement.API/Models/Entities/QuanLyCLBVoThuatContext.cs
@@ -52,8 +52,10 @@ public partial class QuanLyCLBVoThuatContext : DbContext
     public virtual DbSet<VLopHocChiTiet> VLopHocChiTiets { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=VAN;Database=QuanLyCLBVoThuat;Trusted_Connection=True;TrustServerCertificate=True;");
+    {
+        // Connection string is configured in Program.cs from appsettings.json
+        // No hardcoded connection string needed
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
